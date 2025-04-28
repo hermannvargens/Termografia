b6cb937dc74d54d8ac0c5a16e9d40c213ead9dca
Hermann Vargens <96803586+hermannvargens@users.noreply.github.com>
2025-04-28 13:37:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,25 +5,25 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 # Função para plotar histogramas 3D
-def plotar_histogramas_3d(mapa_temperatura_etanol, mapa_temperatura_agua, mapa_temperatura_dec, bins, xinf, xsup, elev, azim, largura_barra):
+def plotar_histogramas_3d(mapa1, mapa2, mapa3, bins, xinf, xsup, elev, azim, largura_barra):
     fig = plt.figure(figsize=(10, 6))
     ax = fig.add_subplot(111, projection='3d')
 
     # Calculando os histogramas
-    n_etanol, bins_etanol = np.histogram(mapa_temperatura_etanol, bins=bins, range=(xinf, xsup))
-    n_agua, bins_agua = np.histogram(mapa_temperatura_agua, bins=bins, range=(xinf, xsup))
-    n_dec, bins_dec = np.histogram(mapa_temperatura_dec, bins=bins, range=(xinf, xsup))
+    n_col1, bins_col1 = np.histogram(mapa1, bins=bins, range=(xinf, xsup))
+    n_col2, bins_col2 = np.histogram(mapa2, bins=bins, range=(xinf, xsup))
+    n_col3, bins_col3 = np.histogram(mapa3, bins=bins, range=(xinf, xsup))
 
     # Definindo a largura das barras
-    width = (bins_etanol[1] - bins_etanol[0])*largura_barra
-    x_etanol = bins_etanol[:-1] + width / 2
-    x_agua = bins_agua[:-1] + width / 2
-    x_dec = bins_dec[:-1] + width / 2
+    width = (bins_col1[1] - bins_col1[0])*largura_barra
+    x_col1 = bins_col1[:-1] + width / 2
+    x_col2 = bins_col2[:-1] + width / 2
+    x_col3 = bins_col3[:-1] + width / 2
 
     # Plota os histogramas 3D
-    ax.bar3d(x_etanol, np.zeros_like(x_etanol), np.zeros_like(x_etanol), width, width, n_etanol, color='blue', edgecolor='black', alpha=0.6, label='Etanol')
-    ax.bar3d(x_agua, np.ones_like(x_agua), np.zeros_like(x_agua), width, width, n_agua, color='yellow', edgecolor='black', alpha=0.6, label='Água')
-    ax.bar3d(x_dec, np.full_like(x_dec, 2), np.zeros_like(x_dec), width, width, n_dec, color='red', edgecolor='black', alpha=0.6, label='DEC')
+    ax.bar3d(x_col1, np.zeros_like(x_col1), np.zeros_like(x_col1), width, width, n_col1, color='blue', edgecolor='black', alpha=0.6, label=col1)
+    ax.bar3d(x_col2, np.ones_like(x_col2), np.zeros_like(x_col2), width, width, n_col2, color='yellow', edgecolor='black', alpha=0.6, label=col2)
+    ax.bar3d(x_col3, np.full_like(x_col3, 2), np.zeros_like(x_col3), width, width, n_col3, color='red', edgecolor='black', alpha=0.6, label=col3)
 
     # Etiquetas
     ax.set_xlabel('Temperatura')
@@ -60,8 +60,8 @@ if uploaded_file is not None:
     elev = st.sidebar.number_input('Elevação', min_value=0, max_value=360, value=30)
     azim = st.sidebar.number_input('Azimute', min_value=0, max_value=360, value=60)
   
-    xinf = st.sidebar.number_input('Valor mínimo (xinf)', value=float(df[[col_etanol, col_agua, col_dec]].min().min()))
-    xsup = st.sidebar.number_input('Valor máximo (xsup)', value=float(df[[col_etanol, col_agua, col_dec]].max().max()))
+    xinf = st.sidebar.number_input('Valor mínimo (xinf)', value=float(df[[col1, col2, col3]].min().min()))
+    xsup = st.sidebar.number_input('Valor máximo (xsup)', value=float(df[[col1, col2, col3]].max().max()))
 
     largura_barra = st.sidebar.number_input('Largura da Barra', min_value=0.0, max_value=1.0, value=0.5)
 
